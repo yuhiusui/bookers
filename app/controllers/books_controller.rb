7d5d0ce7bb16263eebf178d5caf8f879572208d7
vagrant <vagrant@localhost.localdomain>
2020-03-20 02:59:07 +0000

    [add]4-8 エラーとアップデート未実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book), notice: 'Book was successfully created.'
+  	else
+      # flash.now[:alert] = "メッセージ"
+  		render :books
+  	end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update
+  	redirect_to book_path(book), notice: 'Book was successfully updated.'
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: 'Book was successfully destroyed.'
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
