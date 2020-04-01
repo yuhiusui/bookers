a58be1e7b6248c79731691cd89c229a69d78d1b3
yuhiusui <usui.hanshin@gmail.com>
2020-04-01 20:20:31 +0900

    [finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,10 +11,11 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
-  		redirect_to book_path(@book), notice: 'Book was successfully created.'
+  		redirect_to book_path(@book.id), notice: 'Book was successfully created.'
   	else
+      @books = Book.all
       # flash.now[:alert] = "メッセージ"
-  		render :books
+  		render :index
   	end
   end
 
@@ -23,9 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update
-  	redirect_to book_path(book), notice: 'Book was successfully updated.'
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  	  redirect_to books_path, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
